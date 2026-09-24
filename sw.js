b9afa578da26dfa21a45aa9695fc750c2af47c29
ricardopocas-hub <ricardopocas@gmail.com>
2026-09-24 15:39:42 +0100

    BATIMAT PWA v6 - ZIP export
    BATIMAT PWA v6 - ZIP export

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
-const CACHE='batimat26-v5';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
- if(e.request.method!=='GET') return;
- e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
-});
+const CACHE='batimat26-v6';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./sw.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
