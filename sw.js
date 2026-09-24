9e87ab0c2d2820ff74d98d80b6ca8c8350481323
ricardopocas-hub <ricardopocas@gmail.com>
2026-09-24 13:14:13 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='batimat26-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
