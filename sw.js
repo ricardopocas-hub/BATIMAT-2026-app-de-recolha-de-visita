b343aa7b9071a649e586d4230805e361d0e7dc70
ricardopocas-hub <ricardopocas@gmail.com>
2026-09-24 13:41:24 +0100

    Upgrade service worker to version 3 with activation

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-const CACHE='batimat26-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const CACHE='batimat26-v3';
+
+self.addEventListener('install',e=>e.waitUntil(
+  caches.open(CACHE).then(c=>c.addAll([
+    './',
+    './index.html',
+    './manifest.webmanifest',
+    './icon.svg'
+  ]))
+));
+
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>
+    Promise.all(
+      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    )
+  )
+));
+
+self.addEventListener('fetch',e=>e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+));
